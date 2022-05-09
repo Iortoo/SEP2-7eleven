69aef6a075614444aa0af85ec1f941c279f790b4
nanami4yokai <diana.antoniuc@gmail.com>
2022-05-09 14:51:16 +0200

    make funds show in Overview View *tried, not finished*

---
+++ src/BalanceViewModel.java
@@ -0,0 +1,22 @@
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import Client.Model.*;
+
+public class BalanceViewModel {
+
+    private StringProperty updateBalance;
+    private Model model;
+
+    public StringProperty updateBalanceProperty() {
+        return updateBalance;
+    }
+
+    public BalanceViewModel(Model model) {
+        this.model = model;
+        updateBalance = new SimpleStringProperty("Balance: ");
+    }
+
+    public void updateBalance() {
+        updateBalance.setValue("Balance: " + model.getLastUpdateBalance());
+    }
+}
--- src/View/Profile/OverviewViewController.java
+++ src/View/Profile/OverviewViewController.java
@@ -1,5 +1,21 @@
 package View.Profile;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class OverviewViewController
 {
+    @FXML Label balanceLabel;
+
+    private OverviewViewController overviewViewController;
+
+    public OverviewViewController (){
+    }
+
+    public void init(OverviewViewController overviewViewController){
+        this.overviewViewController = overviewViewController;
+
+        balanceLabel.textProperty().bind(overviewViewController.updateBalanceProperty);
+    }
 }
